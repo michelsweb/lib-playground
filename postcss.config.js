80f7dd70031f0d8e36b4942bc961be505753651f
Michael Michel <michael.michel.hh@gmail.com>
2026-09-24 14:23:18 +0200

    Removes comments from generated CSS code via PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [require('@tailwindcss/postcss')],
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('postcss-discard-comments')({ removeAll: true }),
+  ],
 };
